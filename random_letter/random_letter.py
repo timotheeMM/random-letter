ffdaf7b937eb60fc51dd468931d1739cd3a8179b
Timothée MARRERO MEYER <143833750+timotheemm@users.noreply.github.com>
2024-01-21 17:17:32 +0100

    implement the get_random_letter function

--- random_letter/random_letter.py
+++ random_letter/random_letter.py
@@ -1,2 +1,19 @@
-def get_random_letter():
-    return "This is a random letter."
+import random
+import string
+
+def get_random_letter(uppercase: bool = False) -> str:
+    """
+    Generate and return a random letter.
+
+    Args:
+        uppercase (bool, optional): If False, return a lowercase letter.
+                                    If True, return an uppercase letter.
+                                    Defaults to False.
+
+    Returns:
+        str: A random letter.
+    """
+    
+    letters = string.ascii_uppercase if uppercase else string.ascii_lowercase
+    random_letter = random.choice(letters)
+    return random_letter
